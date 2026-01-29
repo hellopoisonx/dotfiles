18a1b85fca9df80f4c0cc72964c71678e3077ffd
hellopoisonx <x1665341912@gmail.com>
2026-01-29 23:00:06 +0800

    neovim && fontconfig

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return -- init.lua:
 {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
 	lazy = true,
 	event = "VimEnter",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,11 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		version = false,
 		build = ":TSUpdate",
-		event = { "VeryLazy" },
-		lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+        lazy = false,
 		init = function(plugin)
-			-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-			-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-			-- no longer trigger the **nvim-treesitter** module to be loaded in time.
-			-- Luckily, the only things that those plugins need are the custom queries, which we make available
-			-- during startup.
-			require("lazy.core.loader").add_to_rtp(plugin)
-			require("nvim-treesitter.query_predicates")
+			-- require("lazy.core.loader").add_to_rtp(plugin)
+			-- require("nvim-treesitter.query_predicates")
 		end,
 		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 		opts_extend = { "ensure_installed" },
@@ -82,10 +75,6 @@ return {
 				},
 			},
 		},
-		---@param opts TSConfig
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
